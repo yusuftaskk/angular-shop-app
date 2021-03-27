29f2f962ba8e9742790dd860b0f871713b29e5b7
yusuftask08 <52704572+yusuftask08@users.noreply.github.com>
2021-03-27 17:33:05 +0300

    Tip güvenliği string,number calıstık..

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from './product';
 
 @Component({
   selector: 'app-product',
@@ -8,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
 export class ProductComponent implements OnInit {
 
   constructor() { }
-  title ="Ürün Listesi" 
-  products : any[] = [
-    {id:1 , name : "laptop", price:2500, categoryId:1, desciription:"monster"} ,
-    {id:2 , name : "mouse", price:25, categoryId:2, desciription:"none"} ,
+  title = "Ürün Listesi"
+  products: Product[] = [
+    { id: 1, name: "laptop", price: 2500, categoryId: 1, description: "monster", imageUrl: "https://cdn.akakce.com/monster/monster-abra-a5-v16-4-i5-9300h-8-gb-250-gb-ssd-gtx1650-15-6-full-hd-notebook-z.jpg" },
+    { id: 2, name: "mouse", price: 25, categoryId: 2, description: "none", imageUrl: "https://cdn.akakce.com/monster/monster-abra-a5-v16-4-i5-9300h-8-gb-250-gb-ssd-gtx1650-15-6-full-hd-notebook-z.jpg" },
+    { id: 1, name: "laptop", price: 2500, categoryId: 1, description: "monster", imageUrl: "https://cdn.akakce.com/monster/monster-abra-a5-v16-4-i5-9300h-8-gb-250-gb-ssd-gtx1650-15-6-full-hd-notebook-z.jpg" },
+    { id: 2, name: "mouse", price: 25, categoryId: 2, description: "none", imageUrl: "https://cdn.akakce.com/monster/monster-abra-a5-v16-4-i5-9300h-8-gb-250-gb-ssd-gtx1650-15-6-full-hd-notebook-z.jpg" },
+    { id: 1, name: "laptop", price: 2500, categoryId: 1, description: "monster", imageUrl: "https://cdn.akakce.com/monster/monster-abra-a5-v16-4-i5-9300h-8-gb-250-gb-ssd-gtx1650-15-6-full-hd-notebook-z.jpg" },
+    { id: 2, name: "mouse", price: 25, categoryId: 2, description: "none", imageUrl: "https://cdn.akakce.com/monster/monster-abra-a5-v16-4-i5-9300h-8-gb-250-gb-ssd-gtx1650-15-6-full-hd-notebook-z.jpg" },
+     { id: 1, name: "laptop", price: 2500, categoryId: 1, description: "monster", imageUrl: "https://cdn.akakce.com/monster/monster-abra-a5-v16-4-i5-9300h-8-gb-250-gb-ssd-gtx1650-15-6-full-hd-notebook-z.jpg" },
+    { id: 2, name: "mouse", price: 25, categoryId: 2, description: "none", imageUrl: "https://cdn.akakce.com/monster/monster-abra-a5-v16-4-i5-9300h-8-gb-250-gb-ssd-gtx1650-15-6-full-hd-notebook-z.jpg" }
   ]
   ngOnInit(): void {
   }
