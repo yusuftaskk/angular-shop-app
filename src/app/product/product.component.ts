12e29f79fc212092bb66c476a2b6bfeba6b5ca93
yusuftask08 <52704572+yusuftask08@users.noreply.github.com>
2021-03-27 17:11:01 +0300

    ng For ile calısmak..

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -9,7 +9,10 @@ export class ProductComponent implements OnInit {
 
   constructor() { }
   title ="Ürün Listesi" 
-  products : any[] 
+  products : any[] = [
+    {id:1 , name : "laptop", price:2500, categoryId:1, desciription:"monster"} ,
+    {id:2 , name : "mouse", price:25, categoryId:2, desciription:"none"} ,
+  ]
   ngOnInit(): void {
   }
 
