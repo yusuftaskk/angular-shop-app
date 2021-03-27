92d73a885f0ee0a041ebfbe0c10538d61e2a7dfc
yusuftask08 <52704572+yusuftask08@users.noreply.github.com>
2021-03-27 17:44:12 +0300

    Kategoriler eklendi...

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from './category';
 
 @Component({
   selector: 'app-category',
@@ -8,7 +9,15 @@ import { Component, OnInit } from '@angular/core';
 export class CategoryComponent implements OnInit {
 
   constructor() { }
+  title="Kategori Listesi"
+  categories : Category[] =[
+    {id:1, name:"Elektronik"},
+    {id:2, name:"Bilgisayar"},
+    {id:3, name:"İletişim"},
+    {id:4, name:"Müzik"},
+    {id:5, name:"Market"}
 
+  ]
   ngOnInit(): void {
   }
 
