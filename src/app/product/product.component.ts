fcb0a6f79162477e84bfced2402c5915e62bf345
yusuftask08 <52704572+yusuftask08@users.noreply.github.com>
2021-03-27 16:27:35 +0300

    componentler olusturuldu..

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+  constructor() { }
+  title ="Ürün Listesi" 
+  products : any[] = [
+    {},
+    {},
+    {},
+    {},
+    {},
+
+  ]
+  ngOnInit(): void {
+  }
+
+}
