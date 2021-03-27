ae6b605cef86f5f4f779562f6090f7f503fd7191
yusuftask08 <52704572+yusuftask08@users.noreply.github.com>
2021-03-27 16:27:46 +0300

    componentler olusturuldu..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoryComponent } from './category/category.component';
+import { NavComponent } from './nav/nav.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent,
+    NavComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
